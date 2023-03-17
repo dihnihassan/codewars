87422787749d5ca14480e437100f939934361584
Dihni Hassan <dihnihassan@gmail.com>
2023-03-17 12:01:20 -0400

    Rock Paper Scissors - part 1 Draw
    e

---
+++ 8kyu/rock-paper-scissors.js
@@ -0,0 +1,11 @@
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+
+const rps = (p1, p2) => {
+
+
+
+  if (p1 === p2 && p2 === p1){
+    return "Draw!"
+  };
+}
