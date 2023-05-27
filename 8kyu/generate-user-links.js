5a042b8d970820e69b371cd6841f87ac827bf51b
Dihni Hassan <dihnihassan@gmail.com>
2023-05-26 23:14:01 -0400

    generate-user-link - complete

---
+++ 8kyu/generate-user-links.js
@@ -0,0 +1,13 @@
+// Your task is to create userlinks for the url, you will be given a username and must return a valid link.
+
+
+function generateLink(user) {
+  
+  let urlEncode = encodeURIComponent(user)
+  
+  let url = `http://www.codewars.com/users/${urlEncode}`;
+  console.log(url);
+  
+return url;
+
+}
